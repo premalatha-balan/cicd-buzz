5320385c6af50d43ab58dd5fa1250760b5d5ef36
premalatha-balan <plbalan@yahoo.com>
2020-03-29 13:29:09 +0100

    heroku.sh was not there before. Added now

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -7,11 +7,5 @@ else
 fi
 docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
 docker push $TRAVIS_REPO_SLUG:$TAG
-#
-#!/bin/sh
-wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-heroku container:login
-heroku container:push web --app $HEROKU_APP_NAME
-heroku container:push web --app python-cicd-buzz
-heroku container:release web --app $HEROKU_APP_NAME
-heroku container:release web --app python-cicd-buzz
+
+

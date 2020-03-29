56b5aa42425e988ebf172e8a26a5c23ec6216f2a
premalatha-balan <plbalan@yahoo.com>
2020-03-29 15:45:10 +0100

    changed the name of the background file

--- buzz/generator.py
+++ buzz/generator.py
@@ -1,10 +1,10 @@
 from __future__ import print_function
 import random
 
-buzz = ('continuous testing', 'continuous integration',
+buzz = ('continuous testing', 'continuous integration', 
     'continuous deployment', 'continuous improvement', 'devops')
 adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
-adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
+adverbs = ('remarkably', 'enormously', 'substantially', 'significantly', 
     'seriously')
 verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
 
@@ -16,9 +16,9 @@ def sample(l, n = 1):
 
 def generate_buzz():
     buzz_terms = sample(buzz, 2)
-    phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs),
+    phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs), 
         sample(verbs), buzz_terms[1]])
     return phrase.title()
 
 if __name__ == "__main__":
-    print(generate_buzz())
+    print generate_buzz()

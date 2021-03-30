c2278f7064515c9d24d0f3e85a5b9b25448fc907
premalatha-balan <plbalan@yahoo.com>
2021-03-30 19:58:57 +0100

    Step 9: heroku app setting up and deploying to heroku

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -7,3 +7,9 @@ else
 fi
 docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
 docker push $TRAVIS_REPO_SLUG:$TAG
+
+#!/bin/sh
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+heroku container:login
+heroku container:push web --app $HEROKU_APP_NAME
+heroku container:release web --app $HEROKU_APP_NAME
